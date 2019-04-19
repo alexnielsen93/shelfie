555dc2d4b230600f796c883d38219effc5f7fce2
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro-2.local>
2019-04-19 11:49:40 -0600

    form functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Dashboard from './Components/Dashboard/Dashboard'
 import Form from './Components/Form/Form'
 import Header from './Components/Header/Header'
-import Product from './Components/Product/Product'
 class App extends Component {
   render() {
     return (
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,16 +1,49 @@
 import React, {Component} from 'react'
 
 export default class Form extends Component{
+  constructor(){
+    super()
+    this.state = {
+      imgUrl: 'img',
+      productName : 'name',
+      productPrice : 0,
 
+    }
+
+  }
+
+  handleChange= (event)=>{
+    let {value, name} = event.target
+    console.log('name',[name], 'value',value)
+    this.setState({
+      [name]: value
+    })
+    console.log(this.state)
+  }
+
+  clearInput =()=>{
+    this.setState({
+      imgUrl: '',
+      productName: '',
+      productPrice: 0,
+    })
+    document.getElementById("userInput").reset()
+
+
+  }
 
   render(){
 
     return <div>
-      <input className = "img-url" placeholder = "image url"></input>
-      <input className = "product-name" placeholder = "product name"></input>
-      <input className = "product-price" placeholder = "price"></input>
-      <button>Cancel</button>
+      <img src = {this.state.imgUrl} onError = {()=>this.src='http://chittagongit.com//images/image-error-icon/image-error-icon-5.jpg'} alt ='' width = "200" height = "200"
+      /> 
+      <form id = "userInput">
+      <input name ="imgUrl" type = "text"  className = "img-url" placeholder = "image url" onChange = {this.handleChange}></input>
+      <input name = "productName" onChange = {this.handleChange}  className = "product-name" placeholder = "product name"></input>
+      <input name = "productPrice"onChange = {this.handleChange} className = "product-price" placeholder = "price"></input>
+      <button onClick = {this.clearInput}>Cancel</button>
       <button>Add</button>
+      </form>
     </div>
 
   }
